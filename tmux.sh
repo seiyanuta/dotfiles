42483e5998387603f143c4d4509b979dd68d5d00
Seiya Nuta <nuta@seiya.me>
2021-06-22 21:25:59 +0900

    Update

--- tmux.sh
+++ tmux.sh
@@ -3,10 +3,3 @@ _fpp() {
   tmux capture-pane -J -S - -E - -b "fpp-$1" -t "$1"
   tmux split-window "tmux show-buffer -b fpp-$1 | fpp || true; tmux delete-buffer -b fpp-$1"
 }
-
-_copy_to_clipboard() {
-  tmux save-buffer - | head 
-  echo
-  printf "\x1b]52;c;$(tmux save-buffer - | base64)\x07"
-  tmux display-message "copied!"
- }
